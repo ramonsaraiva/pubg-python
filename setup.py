5dc989e4d53f219b545a90c9a2e0cdee6f9914da
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-03-12 19:52:50 -0300

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import (
+    find_packages,
+    setup)
+
+setup(
+    name='pubg-python',
+      version='0.1.0',
+      description='A python wrapper for the PUBG developer API',
+      url='https://github.com/ramonsaraiva/pubg-python',
+      author='Ramon Saraiva',
+      author_email='ramonsaraiva@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=('tests*',)),
+      install_requires=[
+        'requests>=2.18.4',
+        'furl>=1.0.1',
+      ],
+)
