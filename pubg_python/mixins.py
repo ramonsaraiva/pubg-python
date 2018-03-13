f5eb8aea963769e0b0621e8ba8d62c2765c18917
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-03-13 10:05:32 -0300

    Fix redundant Accept-Encoding header

--- pubg_python/mixins.py
+++ pubg_python/mixins.py
@@ -9,14 +9,12 @@ class RequestMixin:
 
     BASE_URL = 'https://api.playbattlegrounds.com/'
 
-    def __init__(self, api_key, gzip=False):
+    def __init__(self, api_key):
         self.session = requests.Session()
         self.session.headers.update({
             'Authorization': api_key,
             'Accept': 'application/vnd.api+json'
         })
-        if gzip:
-            self.session.headers['Accept-Encoding'] = 'gzip'
         self.url = furl.furl(self.BASE_URL)
 
 
