95fb49d78de5a144b02f9e06e488f4b93f3fa6f4
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-04-04 20:29:09 -0300

    Add clients module cause they've been growing

--- pubg_python/base.py
+++ pubg_python/base.py
@@ -4,6 +4,10 @@ import furl
 import requests
 
 from . import exceptions
+from .clients import (
+    APIClient,
+    TelemetryClient,
+)
 from .decorators import requires_shard
 from .domain.base import Shard
 from .domain.telemetry.base import Telemetry
@@ -48,43 +52,3 @@ class PUBG:
     def telemetry(self, url):
         data = self.telemetry_client.request(url)
         return Telemetry(data)
-
-
-class Client:
-
-    API_OK = 200
-    API_ERRORS_MAPPING = {
-        401: exceptions.UnauthorizedError,
-        404: exceptions.NotFoundError,
-        415: exceptions.InvalidContentTypeError,
-        429: exceptions.RateLimitError,
-    }
-
-    def __init__(self):
-        self.session = requests.Session()
-        self.session.headers.update({'Accept': 'application/vnd.api+json'})
-        self.url = furl.furl()
-
-    def request(self, endpoint):
-        response = self.session.get(endpoint)
-
-        if response.status_code != self.API_OK:
-            exception = self.API_ERRORS_MAPPING.get(
-                response.status_code, exceptions.APIError)
-            raise exception()
-
-        return json.loads(response.text)
-
-
-class APIClient(Client):
-
-    BASE_URL = 'https://api.playbattlegrounds.com/'
-
-    def __init__(self, api_key):
-        super().__init__()
-        self.session.headers.update({'Authorization': api_key})
-        self.url.set(path=self.BASE_URL)
-
-
-class TelemetryClient(Client):
-    pass
