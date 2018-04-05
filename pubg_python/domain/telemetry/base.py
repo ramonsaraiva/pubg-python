c92488a40625b41025dc9d2b3c0d22e020578990
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-04-05 08:14:27 -0300

    Add events retrieval per type in a telemetry

--- pubg_python/domain/telemetry/base.py
+++ pubg_python/domain/telemetry/base.py
@@ -5,3 +5,6 @@ class Telemetry:
 
     def __init__(self, data):
         self.events = [Event.instance(event) for event in data]
+
+    def events_from_type(self, _type):
+        return [ev for ev in self.events if type(ev).__name__ == _type]
