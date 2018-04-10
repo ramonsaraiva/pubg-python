fc7d1c07985dd0874d5b1e7c056aa7db2c064f88
toda <ytoda@d-forest-corp.com>
2018-04-10 14:21:09 +0900

    Add JP Server.

--- pubg_python/domain/base.py
+++ pubg_python/domain/base.py
@@ -5,11 +5,12 @@ class Shard(Enum):
     PC_AS = 'pc-as'  # Asia
     PC_EU = 'pc-eu'  # Europe
     PC_KAKAO = 'pc-kakao'  # Kakaogames server (Korea only)
-    PC_KRJP = 'pc-krjp'  # Korea/Japan
+    PC_KRJP = 'pc-krjp'  # Korea
     PC_NA = 'pc-na'  # North America
     PC_OC = 'pc-oc'  # Oceania
     PC_SA = 'pc-sa'  # South and Central America
     PC_SEA = 'pc-sea'  # South East Asia
+    PC_JP = 'pc-jp'  # Japan
     XBOX_AS = 'xbox-as'  # Asia
     XBOX_EU = 'xbox-eu'  # Europe
     XBOX_NA = 'xbox-na'  # North America
