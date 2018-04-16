42d1a16d332ee53d5ebe136293bfcd65d34a4da6
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-04-16 12:08:22 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import (
 
 setup(
     name='pubg-python',
-    version='0.2.7',
+    version='0.2.8',
     description='A python wrapper for the PUBG developer API',
     url='https://github.com/ramonsaraiva/pubg-python',
     author='Ramon Saraiva',
