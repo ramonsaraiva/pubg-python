16ecde4439a0f5e63621da82e2ec7ca318497478
Ramon Saraiva <ramonsaraiva@gmail.com>
2019-08-13 12:21:49 -0300

    Fix deprecation warning in dummy tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -9,9 +9,9 @@ class TestDummy(TestCase):
     def test_dummy(self):
         magic = mock.MagicMock()
         magic.cast = 'spell'
-        self.assertEquals(magic.cast, 'spell')
+        self.assertEqual(magic.cast, 'spell')
 
     def test_other_dummy(self):
         magic = mock.MagicMock()
         magic.cast = 'spell'
-        self.assertEquals(magic.cast, 'spell')
+        self.assertEqual(magic.cast, 'spell')
