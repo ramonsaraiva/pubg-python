b7cb478177c24e2011c9065c95c748e79dc9666e
Ramon Saraiva <ramonsaraiva@gmail.com>
2019-09-02 07:54:44 -0300

    Bump version 0.10.0 -> 0.10.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='pubg-python',
-    version='0.10.0',
+    version='0.10.1',
     description='A python wrapper for the PUBG developer API',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
