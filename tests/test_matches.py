e9b3e365926539b935e4ac808ad8db78da0ca890
Stanislav Gelman <glmnio@outlook.com>
2019-09-13 18:47:28 +0300

    :white_check_mark: add matches endpoint tests

---
+++ tests/test_matches.py
@@ -0,0 +1,30 @@
+import json
+import pytest
+import requests_mock
+from pubg_python.base import *
+from pubg_python.domain.base import *
+
+api = PUBG('apikey', Shard.STEAM)
+BASE_URL = APIClient.BASE_URL
+
+@pytest.fixture()
+def mock():
+    with requests_mock.Mocker() as mock:
+        yield mock
+
+@pytest.fixture()
+def match_response():
+    with open('tests/match_response.json') as json_file:
+        yield json.load(json_file)
+
+def test_match_get(mock, match_response):
+    match_id = 'f80126f4-9520-4c66-9198-57820d04bf00'
+    url = '{}shards/steam/matches/{}'.format(BASE_URL, match_id)
+    mock.get(url, json=match_response)
+    match = api.matches().get(match_id)
+    roster = match.rosters[0]
+    participant = roster.participants[0]
+    assert isinstance(match, Match)
+    assert match.id == match_id
+    assert isinstance(roster, Roster)
+    assert isinstance(participant, Participant)
