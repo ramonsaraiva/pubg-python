4d85e28b4315d0194e4ec13626190c054b9a488f
Stanislav Gelman <glmnio@outlook.com>
2019-09-15 02:11:02 +0300

    :white_check_mark: add telemetry base tests

---
+++ tests/test_telemetry.py
@@ -0,0 +1,35 @@
+import json
+import pytest
+import requests_mock
+from pubg_python.base import PUBG, Shard, APIClient, Telemetry
+from pubg_python.domain.telemetry.events import *
+
+api = PUBG('apikey', Shard.STEAM)
+BASE_URL = APIClient.BASE_URL
+TELEMETRY_URL = 'http://telemetry.pubg'
+TELEMETRY_JSON = json.load(open('tests/telemetry_response.json'))
+TELEMETRY_EVENTS_COUNT = 37671
+
+@pytest.fixture()
+def mock():
+    with requests_mock.Mocker() as mock:
+        yield mock
+        
+def test_telemetry_instance(mock):
+    mock.get(TELEMETRY_URL, json=TELEMETRY_JSON)
+    telemetry = api.telemetry(TELEMETRY_URL)
+    assert isinstance(telemetry, Telemetry)
+    assert len(telemetry.events) == TELEMETRY_EVENTS_COUNT
+        
+def test_telemetry_from_json():
+    telemetry = Telemetry.from_json('tests/telemetry_response.json')
+    assert isinstance(telemetry, Telemetry)
+    assert len(telemetry.events) == TELEMETRY_EVENTS_COUNT
+
+def test_events_from_type(mock):
+    mock.get(TELEMETRY_URL, json=TELEMETRY_JSON)
+    telemetry = api.telemetry(TELEMETRY_URL)
+    match_definition = telemetry.events_from_type('LogMatchDefinition')
+    assert isinstance(match_definition[0], LogMatchDefinition)
+    assert len(match_definition) == 1
+    
